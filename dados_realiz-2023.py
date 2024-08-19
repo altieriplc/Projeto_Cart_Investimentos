5fe4ea1d14a0d12cb76b42951f3bdd0937693aa9
Altieri Palorca <altierirpalorca@gmail.com>
2024-08-19 10:18:27 -0300

    1.3.2 tratamento com python(pandas)

--- dados_realiz-2023.py
+++ dados_realiz-2023.py
@@ -18,15 +18,16 @@ dados_caixa_2023_df = dados_caixa_2023_df.rename(columns={'Unnamed: 0': 'Ativos'
 
 
 # renomeia diversas linhas
-dados_caixa_2023_df.loc[[0, 2, 5, 14],'Ativos'] = [
+dados_caixa_2023_df.loc[[0, 2, 4, 5, 14],'Ativos'] = [
     'Total Geral Mensal', 
     'Total Renda Fixa',
+    'Outros Renda Fixa',
     "Total Fii's",
     'Total Div Ações'
     ]
 
 # remove primeiros 5 caracteres das palavras
-dados_caixa_2023_df.loc[~dados_caixa_2023_df.index.isin([0, 2, 5, 14]),'Ativos'] = dados_caixa_2023_df['Ativos'].str[5:]
+dados_caixa_2023_df.loc[~dados_caixa_2023_df.index.isin([0, 2, 4, 5, 14]),'Ativos'] = dados_caixa_2023_df['Ativos'].str[5:]
 
 # cria coluna média
 dados_caixa_2023_df['Média'] = (dados_caixa_2023_df.sum(axis=1, numeric_only=True)/12).round(2)
@@ -42,7 +43,7 @@ novas_linhas = pd.DataFrame([
     ['Bitcoin'] + [0] * (len(dados_caixa_2023_df.columns) - 1)
 ], columns=dados_caixa_2023_df.columns)
 
-# Concatene as novas linhas ao DataFrame original
+# Concatena as novas linhas ao DataFrame original
 dados_caixa_2023_df = pd.concat([dados_caixa_2023_df, novas_linhas], ignore_index=True)
 
 # renomeia uma coluna
@@ -58,12 +59,18 @@ dados_caixa_2023_df = dados_caixa_2023_df.drop(columns=['Total Anual'])
 dados_caixa_2023_df['Total Anual'] = dados_caixa_2023_df.drop(columns=['Média']).sum(
     axis=1, numeric_only=True).round(2)
 
+#localiza as linhas desejadas
+#agrupa_renda_fixa = dados_caixa_2023_df.loc[[2, 3]].sum()
+
+
+
+# ------------------------------------- Exportação GITHUB ------------------------------------ #
 # r é para que o python entenda as barras invertidas no caminho
 caminho_arquivo = r'C:\Users\altie\OneDrive\Altieri\Softwares\Dev\Projetos Pessoais\Projeto_Carteira_Investimento\dados_realiz-2023_tratados.xlsx'
 
 # index false para o indice não ser exportado
 dados_caixa_2023_df.to_excel(caminho_arquivo, index=False)
 #print(dados_caixa_2023_df['Anual'])
-
+# ------------------------------------- Exportação GITHUB ------------------------------------ #
 
 print(dados_caixa_2023_df)
